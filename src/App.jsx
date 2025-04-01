36aae5fd0be3a6c28900eacb25b114301c03e87a
Dmytro Kopaihora <47076154+Dimonjik777@users.noreply.github.com>
2025-04-01 08:34:05 +0300

    Add basic API fetch
    And add basic render movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,46 @@
-import {useState} from "react"
+import { useState, useEffect } from "react"
 import SearchMovies from "./components/SearchMovies"
 
 function App() {
 
+  // search input data
   const [searchQuery, setSearchQuery] = useState("");
-
-  function handleChangeSearch(event){
+  function handleChangeSearch(event) {
     setSearchQuery(event.target.value);
   }
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://www.omdbapi.com/?s=${searchQuery}&apikey=1ae6eafe`)
+      .then(response => response.json())
+      .then(json => {
+        if (json.Response == "True")
+          setMovies(json.Search)
+        else { 
+          setMovies([]);
+        }
+      }
+      )
+      .catch(error => console.error(error))
+  }, [searchQuery]);
 
   return (
     <>
       <div className="container">
         <SearchMovies search={searchQuery} onChangeSearch={handleChangeSearch} />
+
+        <div className="movies">
+          {
+            movies.length != 0 &&
+              movies.map(element => (
+                <div className="movie" key={element.imdbID}>
+                  <img className="movie__img" src={element.Poster} alt={element.Title} />
+                  <h2 className="movie__title">{element.Title}</h2>
+                </div>
+              ))
+          }
+        </div>
       </div>
     </>
   )
